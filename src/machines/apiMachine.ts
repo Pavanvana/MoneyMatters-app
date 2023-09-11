07cb5231f4a50d71d5dfd1de6d65497d0f4b4836
Pavan <vana.pavankalyan1999@gmail.com>
2023-09-11 14:05:44 +0530

    xstateReviewUpdates

--- src/machines/apiMachine.ts
+++ src/machines/apiMachine.ts
@@ -1,7 +1,21 @@
 import { assign, createMachine } from "xstate";
 
+interface ResponseData{
+    id: number;
+    transaction_name: string;
+    type: string;
+    category: string;
+    amount: number;
+    date: Date|string;
+    user_id: string|undefined;
+}
+  
+interface Response {
+    transactions: Array<ResponseData>
+}
+
 export const apiMachine = createMachine({
-    /** @xstate-layout N4IgpgJg5mDOIC5QEMAOBLAssgxgC3QDswA6AGwHtkIioBiCC4kogNwoGtS0tcDnK1WgjYUcyAC7omAbQAMAXXkLEoVBVjopTVSAAeiAKwAWABwlDc08YBMAZkM2AbE4DsT0wBoQAT0T25EjljOQBGJztQuWdTYJsAX3jvHmx8IlJBGkJ6MAAnXIpcklQySQAzQoBbEhS+dPIqLKgRQnZxbUJlZV11TQ7dAwRDOycSAE5rO1NQibcbd28-BFc7ElMx4ydQ6ZdTO1dwxOSMVP5SPILcugAlAFEAFWuATW6kEF6taUIBoyDXebGrlMThssRC+0Mi0QAFoxhZQsZbB4ots7IDjEcQLU0sx0BAyGA6AAxB4AYQAEq81BpPjo3oMPGsbKFwqZYu4bLYxlCEKFDK4glZ-qZDGN0XJ-okkiBCBQIHBdNizj0af16TDQsySKERuEHK45GFXMZub4YYCSMzEXyxoYnIj7JilfVMrQVX0vj8EDYbIFQgd1tY2WM5LaeZqbCQzIZRXInKG9pEMdLncxYABXHA4ODwN4fNWgQbMwwkVxAvViqYi4Y80El4HrQ3OKKmA5Ok51ZgXQru2nfdXe33agMTMyNsNmhCw8YI4zbYYbVxjJwTdu8HGkPEE3sF-Qwlba3URfmG-0m8N2kjx1szUWg-22qXxIA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QEMAOBLAssgxgC3QDswA6AGwHtkIioBiCC4kogNwoGtS0tcDnK1WgjYUcyAC7omAbQAMAXXkLEoVBVjopTVSAAeiAKwAWABwlDc08YBMAZkM2AbE4DsT0wBoQAT0T25EjljOQBGJztQuWdTYJsAX3jvHmx8IlJBGkJ6MAAnXIpcklQySQAzQoBbEhS+dPIqLKgRQnZxbUJlZV11TQ7dAwRDOycSAE5rO1NQibcbd28-BFc7ElMx4ydQ6ZdTO1dwxOSMVP5SPILcugAlAFEAFWuATW6kEF6taUIBoyDXebGrlMThssRC+0Mi0QAFoxhZQsZbB4ots7IDjEcQLU0sx0BAyGA6AAxB4AYQAEq81BpPjo3oMPGsbKFwqZYu4bLYxlCEKFDK4glZ-qZDGN0XJ-okkiBCBQIHBdNizj0af16TDQsySKERuEHK45GFXMZub4YYCSMzERLHJZNq5MUr6plaCq+l8fggbDZAqEDutrGyxnIxpCzbzvSQzIZRXInCG9pEMdKncxYABXHA4ODwN4fNWgQbMwwkVxAvViqYi4Y80El4HrQ3OKKmA6Ok51ZgXQpu2nfdVen3a-0TMyN0M82HjBHGbbDDauMZOCbt3g40h4gm9gv6GErbW6iL8w1+k08vmjeOtmai0F+0NS+JAA */
     id: 'apiMachine',
     initial: "idle",
     tsTypes: {} as import("./apiMachine.typegen").Typegen0,
@@ -14,7 +28,7 @@ export const apiMachine = createMachine({
     schema: {
         services: {} as {
             FETCH_DATA: {
-                data: any
+                data: Response
             }
         },
         events: {} as {
@@ -41,7 +55,9 @@ export const apiMachine = createMachine({
             }
         },
 
-        success: {},
+        success: {
+            type: 'final'
+        },
 
         error: {
             on: {
